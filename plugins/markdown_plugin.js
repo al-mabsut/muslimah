8698e8fd2f62a01fc4fee844b64f0b9f12ab92cd
Mohamed Ahraf <mohamed7elprans@yahoo.com>
2024-01-21 18:23:17 +0200

    Replace react-markdown with custom parsing logic
    * First we removed `react-markdown` because we had many problems to connect it with our main project `MyhaydApp` as it was throwing a lot of errors, despite numerous attempts to resolve errors ( mostly because of old version of preact-cli or a conflict with other library ) preventing its successful integration, so connecting `react-markdown` proved to be problematic.
    * As an alternative, we explored another library `markdown-to-jsx` but it was throwing weird error `Can't read properties of undefined 'replace'` when loading some components in our main app `MyhaydApp` ( even though it was working fine alone without loading it in `muslimah` package ).
    * To address these challenges and ensure a consistent solution across both projects, we opted for a custom parsing logic, the 'prepareTextElements' function now serves as a lightweight and reliable method for converting Markdown content from JSON strings to HTML code.
    * This decision was guided by the need for a more robust, easy and flexible approach that aligns with our specific project requirements.
    * The new custom code not only resolves the issues faced with external libraries but also provides improved control and performance, offering a tailored solution for rendering markdown content our package `muslimah`.
    * Beside that we have fixed the lists hierarchy of the paragraphs ( now it can only contain 2 ul lists )
    * Also we have introduced some tests for the `parser` function in our markdown_plugin and for 'prepareTextElements' function.

--- plugins/markdown_plugin.js
+++ plugins/markdown_plugin.js
@@ -3,32 +3,72 @@ import fs from 'fs';
 import { h } from 'preact';
 // import { marked } from 'marked';
 
-const parser = (md) => {
+/*
+  **Inputs:**
+    - `md` (string): A Markdown file represented as a string.
+
+  **Markdown Syntax Requirements:**
+    - The title should be formatted as `# Ruling: our Title can be here` ( Ensure there is a space after `# Ruling` )
+    - Every paragraph must be placed after an h2 heading (`##`). h2 serves as the key for the paragraph content
+    - Inner paragraph (i.e '  - some text here') must contain two spaces before `-`
+  
+  **Output:**
+    A JSON object with the following structure:
+    {
+      title: 'our Title can be here',
+      'h2' : [
+        'normal paragraph',
+        'in:paragraph that is in an internal list'
+      ],
+      'another h2' : [
+        'normal paragraph',
+        'in:paragraph that is in an internal list'
+      ]
+    }
+*/
+export const parser = (md) => {
+  if ( !md || typeof(md) !== 'string' ) {
+    throw Error('Markdown must be a string!');
+  }
   const startToken = '# Ruling:';
   const headingToken = '##';
   const lines = md.split('\n');
   const jsonResult = {};
   
   for (let i = 0; i < lines.length; i++) {
-    const line = lines[i].trim();
-    
-    if (line.startsWith(startToken)) {
-      const title = line.replace(`${startToken} `, '');
-      jsonResult.title = `# ${title}`;
+    const line = lines[i];
+    if (line.trim().startsWith(startToken)) {
+      const title = line.replace(`${startToken} `, '').trim();
+      jsonResult.title = title;
     }
 
-    if (line.startsWith(headingToken)) {
-      const subtitle = line.replace(`${headingToken} `, '');
-      jsonResult[subtitle] = '';
+    if (line.trim().startsWith(headingToken)) {
+      const subtitle = line.replace(`${headingToken} `, '').trim();
+      jsonResult[subtitle] = [];
     }
-    else if (line.startsWith('-')) {
+    else if ( line.startsWith('  -') ) {
+      const content = line.replace(' -', '').trim();
+      const keys = Object.keys(jsonResult);
+      const lastKey = keys[keys.length - 1];
+      if ( !jsonResult[lastKey] ) {
+        throw Error('Paragraph must be after an h2 ## title');
+      }
+      jsonResult[lastKey].push(`in:${content}`);
+    }
+    else if (line.trim().startsWith('-')) {
       const content = line.replace('-', '').trim();
       const keys = Object.keys(jsonResult);
       const lastKey = keys[keys.length - 1];
-      jsonResult[lastKey] += `- ${content} \n`;
+      if ( !jsonResult[lastKey] ) {
+        throw Error('Paragraph must be after an h2 ## title');
+      }
+      jsonResult[lastKey].push(content);
     }
   }
 
+  if ( !jsonResult?.title ) {
+    throw Error('Markdown must have a title!');
+  }
   return jsonResult;
 };
 
